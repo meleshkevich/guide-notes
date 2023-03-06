e91e21f6d61ebfa045942eee8e928480ef7fbbb0
Ilja M <i.meleshkevich@gmail.com>
2023-03-06 23:10:55 +0100

    added inputs

---
+++ components/Notes/Form.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+const notesInput = reactive({
+  title: "",
+  note: "",
+});
+</script>
+<template>
+  <NCard class="card">
+    <input v-model="notesInput.title" placeholder="Notes title" />
+    <textarea v-model="notesInput.note" placeholder="My notes"></textarea>
+    <NButton>Save notes</NButton>
+  </NCard>
+</template>
+<style scoped>
+.card {
+  padding: 1.5rem;
+  display: flex;
+  flex-direction: column;
+}
+input,
+textarea {
+  border: 0.1rem solid rgba(0, 0, 0, 0.1);
+  margin-bottom: 0.5rem;
+  border-radius: 0.25rem;
+  padding: 0.25rem;
+}
+</style>
--- pages/profile.vue
+++ pages/profile.vue
@@ -3,6 +3,20 @@ definePageMeta({ middleware: "auth" });
 </script>
 <template>
   <div>
-    <h1>Main Profile</h1>
+    <div class="container">
+      <h3>My Notes</h3>
+      <NotesForm />
+    </div>
   </div>
 </template>
+<style scoped>
+.container {
+  max-width: 50%;
+  margin: 0 auto;
+  padding: 3rem 0;
+  height: 90vh;
+}
+h3 {
+  font-size: 2rem;
+}
+</style>
